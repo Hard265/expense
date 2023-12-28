124befddc6bc7570387e0ed1ad9ed760556c2a2e
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2023-12-29 01:06:15 +0300

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,16 @@
-import { Tabs } from 'expo-router';
+import { Tabs } from 'expo-router/tabs';
 
-export default function Layout() {
-  return <Tabs />;
+export default function AppLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        // Name of the dynamic route.
+        name="home"
+        options={{
+          // Ensure the tab always links to the same href.
+          href: '/evanbacon',
+        }}
+      />
+    </Tabs>
+  );
 }
