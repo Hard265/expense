d60ac83576fd2cd0b0bb353ad623c96bc76787e0
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2023-12-30 05:18:43 +0000

    remake

--- app/index.js
+++ app/index.js
@@ -1,5 +1,34 @@
-import { Text } from 'react-native';
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Page() {
-  return <Text>Home page</Text>;
-}
+const WelcomeScreen = () => {
+  const navigation = useRouter();
+
+    return (
+        <View style={styles.container}>
+              <Text style={styles.title}>Cat X Dog</Text>
+                    <Text style={styles.subtitle}>Welcome</Text>
+                          <Button title="Cats tab" onPress={() => navigation.push("/cats")} />
+                              </View>
+                                );
+                                };
+
+                                const styles = StyleSheet.create({
+                                  container: {
+                                      flex: 1,
+                                          alignItems: "center",
+                                              justifyContent: "center",
+                                                  backgroundColor: "#fff",
+                                                    },
+                                                      title: {
+                                                          fontSize: 24,
+                                                              fontWeight: "bold",
+                                                                  marginBottom: 10,
+                                                                    },
+                                                                      subtitle: {
+                                                                          fontSize: 16,
+                                                                              textAlign: "center",
+                                                                                },
+                                                                                });
+
+                                                                                export default WelcomeScreen;
